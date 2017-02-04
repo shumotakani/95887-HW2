28669c0eaf12fd509e808718aea60b7540e3ea4e
Shu Wang <oneshoe0411@gmail.com>
2017-02-04 00:12:17 -0500

    first commit

---
+++ bin/static/js/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var app = angular.module('app', [
+    'ui.router',
+    'ngResource',
+    'app.services',
+    'app.controllers'
+
+]);
+
+app.config(function($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+        .state("quote", {
+            url: "/",
+            templateUrl : "templates/quote.html",
+            controller: "RandomQuoteCtrl"
+        })
+        .state("add", {
+            url: "/add",
+            templateUrl : "templates/add.html",
+            controller: "SaveQuoteCtrl"
+        })
+	    .state("list",{
+	        url: "/list",
+	        templateUrl : "templates/list.html",
+	        controller: "ListQuoteCtrl"
+	    });
+});
